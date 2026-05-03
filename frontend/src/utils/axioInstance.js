e9676d6042a6aad157e97a088a64bdd8223dd657
kartik chauhan <kartik1642005@gmail.com>
2026-05-04 01:45:15 +0530

    Added backend + fixed CORS

--- frontend/src/utils/axioInstance.js
+++ frontend/src/utils/axioInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const BASE_URL = "http://localhost:3000/api"
+const BASE_URL = "https://task-manager-app-erkv.onrender.com/api"
 
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
